ab8d378bdd3e493d73cef2f0f2c7d93bf9002143
ankit <ankitchouhan1020@gmail.com>
2018-07-09 21:05:15 +0530

    margin managed

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       fixed
       app
     >
-      <v-list dense>
+      <v-list >
         <v-list-tile @click="" v-for="item in menuItems" :key="item.title" router :to="item.path">
           <v-list-tile-action >
             <v-icon>{{item.icon}}</v-icon>
@@ -21,11 +21,10 @@
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>Eureka</v-toolbar-title>
     </v-toolbar>
-    <v-container>
-      <main class="ma-4">
+
+      <main>
         <router-view></router-view>
       </main>
-    </v-container>
 
     <v-footer app fixed>
       <span>&copy; 2017</span>
